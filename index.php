1f0d16a8e55510f20c14bacadb9f8223020f32b1
wyzkkk <2602478787@qq.com>
2017-03-22 21:32:56 +0800

    index.php add class

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
 <?php
 echo 111;
+class notest{
+	public $age;
+	public $name;
+	public function __construct($age, $name){
+		$this->age = $age;
+		$this->name = $name;
+	}
+	public function index(){
+		echo 'index111';
+	}
+	public function ajax(){
+		echo 'json222';
+	}
+
+}
